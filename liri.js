5efffb026251b17f634915b9fedee7063e02cef7
kjige <chrisleongsc2@gmail.com>
2016-12-16 17:53:28 -0600

    adding comments and logging

--- liri.js
+++ liri.js
@@ -1,14 +1,14 @@
-// At the top of the liri.js file, write the code you need to grab the data from keys.js. Then store the keys in a variable.
 var fs = require('fs');
 var inquirer = require('inquirer');
 var Twitter = require('twitter');
 var spotify = require('spotify');
 var request = require('request');
 var twitterKeys = require('./keys.js');
-// var randomText = require('./random.txt');
-// var randomTextsplit = randomText.split(',');
-// console.log(randomTextsplit);
+
+// default song if none entered
 var song = 'The Sign Ace of Base';
+
+// default movie if none entered
 var movieTitle = 'Mr Nobody';
 var url = 'http://www.omdbapi.com/?tomatoes=true&t=' + movieTitle;
 var query;
@@ -23,6 +23,7 @@ var client = new Twitter({
     access_token_key: twitterKeys.twitterKeys.access_token_key,
     access_token_secret: twitterKeys.twitterKeys.access_token_secret
 });
+
 inquirer.prompt([{
     type: 'list',
     message: 'choose one',
@@ -82,88 +83,135 @@ function whatSong() {
     });
 }
 
+// ask user for movie title
 function whatMovie() {
     inquirer.prompt([{
         type: 'input',
         message: 'What is the movie title?',
         name: 'movieTitle'
     }]).then(function (resp) {
+
+        // check that user did not enter empty query
         if (resp.movieTitle.length > 0) {
+
+            // store user query for logging to log.txt
             queryLog = resp.movieTitle;
+
+            // store movie title
             movieTitle = resp.movieTitle;
+
+            // make url with user input title
             url = 'http://www.omdbapi.com/?tomatoes=true&t=' + movieTitle;
         } else {
+            // if user input is empty, use default movie title
             queryLog = movieTitle;
         }
         omdbAPI();
     });
 }
 
+// API call to Twitter
 function getTweets() {
     client.get('statuses/user_timeline', {
         count: '20'
     }, function (error, tweets, response) {
-        if (error) throw error;
-        for (var i = 0; i < tweets.length; i++) {
-            console.log(tweets[i].text);
+        var summary;
+
+        // return error message if error occurs
+        if (error) {
+            console.log('Error occurred: ' + error);
+            return;
+        } else {
+
+            // store last 20 tweets to summary
+            for (var i = 0; i < tweets.length; i++) {
+                summary += '\n\t' + tweets[i].text;
+            }
         }
-        queryLog = '';
-        logActivity();
+
+        // display summary
+        console.log(summary);
+
+        // call this function with summarized data
+        logActivity(summary);
     });
 }
 
+// API call to Spotify
 function spotifySearch() {
     spotify.search({
         type: 'track',
         query: song
-    }, function (err, data) {
-        if (err) {
-            console.log('Error occurred: ' + err);
+    }, function (error, data) {
+
+        // return error message if error occurs
+        if (error) {
+            console.log('Error occurred: ' + error);
             return;
+        } else {
+
+            // select and store specific data from object
+            var a = "Artist: " + data.tracks.items[0].album.artists[0].name;
+            var s = "Song: " + data.tracks.items[0].name;
+            var u = "URL: " + data.tracks.items[0].preview_url;
+            var al = "Album: " + data.tracks.items[0].album.name;
+
+            // concatenate stored data and display in terminal
+            var summary = '\t' + a + '\n\t' + s + '\n\t' + u + '\n\t' + al;
+            console.log(summary);
+
+            // call this function with summarized data
+            logActivity(summary);
         }
-        // console.log(JSON.stringify(data.tracks.items[0], null, 2));
-        console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
-        console.log("Song: " + data.tracks.items[0].name);
-        console.log("URL: " + data.tracks.items[0].preview_url);
-        console.log("Album: " + data.tracks.items[0].album.name);
-        logActivity();
     });
 }
 
+// API call for OMDB 
 function omdbAPI() {
     request(url, function (error, response, body) {
-        var resp = JSON.parse(body);
-        // console.log(resp);
-        console.log('Title: ' + resp.Title);
-        console.log('Year: ' + resp.Year);
-        console.log('IMDB Rating:' + resp.imdbRating);
-        console.log('Country: ' + resp.Country);
-        console.log('Language: ' + resp.Language);
-        console.log('Plot:' + resp.Plot);
-        console.log('Actors: ' + resp.Actors);
-        console.log('Title: ' + resp.Title);
-        console.log('Rotten Tomatoes Rating: ' + resp.tomatoRating);
-        console.log('Rotten Tomatoes URL: ' + resp.tomatoURL);
-        logActivity();
+
+        // return error message if error occurs
+        if (error) {
+            console.log('Error occurred: ' + error);
+            return;
+        } else {
+
+            // convert response to JSON object
+            var resp = JSON.parse(body);
+
+            // select and store specific data from object
+            var t = 'Title: ' + resp.Title;
+            var y = 'Year: ' + resp.Year;
+            var i = 'IMDB Rating:' + resp.imdbRating;
+            var c = 'Country: ' + resp.Country;
+            var l = 'Language: ' + resp.Language;
+            var p = 'Plot:' + resp.Plot;
+            var a = 'Actors: ' + resp.Actors;
+            var r = 'Rotten Tomatoes Rating: ' + resp.tomatoRating;
+            var ru = 'Rotten Tomatoes URL: ' + resp.tomatoURL;
+
+            // concatenate stored data and display in terminal
+            var summary = '\t' + t + '\n\t' + y + '\n\t' + i + '\n\t' + c + '\n\t' + l + '\n\t' + p + '\n\t' + a + '\n\t' + r + '\n\t' + ru;
+            console.log(summary);
+
+            // call this function with summarized data
+            logActivity(summary);
+        }
     });
 }
 
 function randomText() {
     var random = fs.readFile('random.txt', 'utf8', function (err, data) {
-        // console.log(data);
         var dataArray = data.split(',');
         action = dataArray[0];
         query = dataArray[1];
-        // console.log(action);
-        // console.log(query);
         randomCheck = true;
-        // console.log(randomCheck);
-        // console.log(dataArray);
         checkChoice();
     });
 }
 
-function logActivity() {
-    log = '\n' + action + ',' + queryLog;
+// log activity in log.txt
+function logActivity(summary) {
+    log = '\n\nAction chosen: ' + action + ', query: ' + queryLog + '\n' + summary;
     fs.appendFile('log.txt', log, function () {});
 }
